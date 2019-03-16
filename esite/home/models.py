6d9056694d89aae5cbc7e226597dd5efc9f33090
kleberbaum <kleberbaum@erebos.xyz>
2019-03-16 20:15:31 +0100

    test

--- esite/home/models.py
+++ esite/home/models.py
@@ -200,7 +200,7 @@ class HomePage(Page):
               ('img', ImageChooserBlock(required=False, classname="full")),
               ('quote', blocks.RichTextBlock(blank=True, features=['bold', 'italic', 'underline', 'strikethrough', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'hr', 'embed', 'link', 'document-link', 'image'], classname="full")),
               ('name', blocks.CharBlock(blank=True, classname="full")),
-              ('title', blocks.CharBlock(blank=True, classname="full"))
+              ('info', blocks.CharBlock(blank=True, classname="full"))
             ]))
           ])),
           ('button', blocks.StructBlock([
